0072701bbd3be602ee11422c25a36191ad4799e6
julienturck <julien.turck.tic@gmail.com>
2024-05-06 14:28:35 +0200

    add router and pages + finish footer

---
+++ src/pages/Accueil.jsx
@@ -0,0 +1,11 @@
+const Accueil = () => {
+    return (
+        <div>
+            <h1>Bonjour, je suis John Doe</h1>
+            <h2>Développeur web full stack</h2>
+            <button>En savoir plus</button>
+        </div>
+    )
+}
+
+export default Accueil;
