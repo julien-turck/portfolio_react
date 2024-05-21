a3a96776612c650f6722d67be9d32c30a193818f
julienturck <julien.turck.tic@gmail.com>
2024-05-21 15:45:40 +0200

    add page mentions + custom css

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -15,49 +15,56 @@ const Contacts = () => {
             <Card>
                 <CardBody>
                     <section id='intro'>
-                        <h3 className='titleSection'>me contacter</h3>
+                        <h3 id='titleSection'>me contacter</h3>
                         <p>Pour me contacter en vue d'un entretien ou d'une future collaboration, merci de remplir le formulaire de contact.</p>
-                        <div className='hr'></div>
+                        <div id='hr'></div>
                     </section>
-                    <Container>
+                    <Container id='containerForm'>
                         <Row>
                             <Col md={6}>
-                                <h4 className='sectionTitle'>Formulaire de contact</h4>
-                                <div className='hr'></div>
+                                <div>
+                                    <h4 className='sectionTitle'>Formulaire de contact</h4>
+                                    <div className='hrForm'></div>
+                                </div>
                                 <Form>
                                     <Form.Group>
-                                        <Form.Control className='label' type='lastname' placeholder='Votre nom' />
+                                        <Form.Control className='label' type='lastname' placeholder='Votre nom' required/>
                                     </Form.Group>
                                     <Form.Group>
-                                        <Form.Control className='label' type='email' placeholder='Votre adresse mail' />
+                                        <Form.Control className='label' type='email' placeholder='Votre adresse mail' required />
                                     </Form.Group>
                                     <Form.Group>
-                                        <Form.Control className='label' type='number_phonr' placeholder='Votre numéro de téléphone' />
+                                        <Form.Control className='label' type='number_phone' placeholder='Votre numéro de téléphone' required />
                                     </Form.Group>
                                     <Form.Group>
-                                        <Form.Control className='label' type='object' placeholder='Sujet' />
+                                        <Form.Control className='label' type='object' placeholder='Sujet' required />
                                     </Form.Group>
                                     <Form.Group>
-                                        <Form.Control className='label' as="textarea" placeholder='Votre message' />
+                                        <Form.Control className='label' as="textarea" placeholder='Votre message' required />
                                     </Form.Group>
                                     <Button variant='primary' type='submit'>Envoyer</Button>
                                 </Form>
                             </Col>
                             <Col md={6}>
-                                <h4 className='sectionTitle'>Mes coordonnées</h4>
-                                <div className='hr'></div>
-                                <div className='coordonnées'>
+                                <div>
+                                    <h4 className='sectionTitle'>Mes coordonnées</h4>
+                                    <div className='hrForm'></div>
+                                </div>
+                                <div className='coordonnéesContact'>
                                     <figure className='iconeContain'>
                                         <img className='icone' src={position} alt='icone de position'></img>
                                     </figure>
                                     <p>40 rue Laure Diebold, 69009 Lyon, France</p>                                    
                                 </div>
-                                <div className='coordonnées'>                                    
+                                <div className='coordonnéesContact'>                                    
                                     <figure className='iconeContain'>
                                         <img className='icone' src={telephone} alt='icone de téléphone'></img>
                                     </figure>
-                                    <p>06 20 30 40 50</p>                                                                     
-                                </div>             
+                                    <p>06 20 30 40 50</p>                                                                                              
+                                </div>
+                                <div>
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2782.626965754016!2d4.796403975485163!3d45.77866571240328!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4eb65edac5b3f%3A0xe01c47049cb2e2b9!2s40%20Rue%20Laure%20Diebold%2C%2069009%20Lyon!5e0!3m2!1sfr!2sfr!4v1716297791843!5m2!1sfr!2sfr" width="100%" height="100%" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                </div>                                             
                             </Col>
                         </Row>
                     </Container>
--- src/pages/Mentions.jsx
+++ src/pages/Mentions.jsx
@@ -3,59 +3,68 @@ import position from '../img/position.svg';
 import telephone from '../img/telephone.svg';
 import enveloppe from '../img/enveloppe.svg';
 import globe from '../img/globe.svg';
+import '../css/Mentions.css'
+import Container from 'react-bootstrap/Container';
 
 function BasicExample() {
   return (
-    <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Editeur du site</Accordion.Header>
-        <Accordion.Body>
-          <p>John Doe</p>
-          <div className='coordonnées'>
+    <Container fluid id='mentions'>
+      <section id='intro'>
+        <h3 id='titleSection'>mentions légales</h3>
+        <div id='hr'></div>
+      </section>        
+      <section id='body'>
+        <Accordion id='accordion'defaultActiveKey="0">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Editeur du site</Accordion.Header>
+            <Accordion.Body className='body'>
+              <h4 className='mentionsTitle'>John Doe</h4>
+              <div className='coordonnéesMentions'>
+                  <figure className='iconeContain'>
+                      <img className='icone' src={position} alt='icone de position'></img>
+                  </figure>
+                  <p>40 rue Laure Diebold</p>                                    
+              </div>
+              <p>69009 Lyon, France</p>
+              <div className='coordonnéesMentions'>                                    
+                    <figure className='iconeContain'>
+                        <img className='icone' src={telephone} alt='icone de téléphone'></img>
+                    </figure>
+                    <p><a className='linkMentions' href="tel:0620304050">06 20 30 40 50</a></p>                                                                    
+                </div>
+                <div className='coordonnéesMentions'>                                    
+                    <figure className='iconeContain'>
+                        <img className='icone' src={enveloppe} alt="icone d'une enveloppe"></img>
+                    </figure>
+                    <p><a className='linkMentions' href="mailto:john.doe@gmail.com">john.doe@gmail.com</a></p>                                                                                  
+                </div>          
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Hébergeur</Accordion.Header>
+            <Accordion.Body className='body'>
+              <h4 className='mentionsTitle'>Always data</h4>
+              <p>91 rue du Faubourg Saint Honoré</p>
+              <p>75008 Paris</p>
+              <div className='coordonnéesMentions'>
                 <figure className='iconeContain'>
-                    <img className='icone' src={position} alt='icone de position'></img>
+                  <img className='icone' src={globe} alt="icone d'un globe"></img>
                 </figure>
-                <p>40 rue Laure Diebold, 69009 Lyon, France</p>                                    
-            </div>
-          <p>69009 Lyon, France</p>
-          <div className='coordonnées'>                                    
-                <figure className='iconeContain'>
-                    <img className='icone' src={telephone} alt='icone de téléphone'></img>
-                </figure>
-                <p>06 20 30 40 50</p>                                                                     
-            </div>
-            <div className='coordonnées'>                                    
-                <figure className='iconeContain'>
-                    <img className='icone' src={enveloppe} alt="icone d'une enveloppe"></img>
-                </figure>
-                <p>john.doe@gmail.com</p>                                                                     
-            </div>          
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Hébergeur</Accordion.Header>
-        <Accordion.Body>
-          <p>Always data</p>
-          <p>91 rue du Faubourg Saint Honoré</p>
-          <p>75008 Paris</p>
-          <div className='coordonnées'>                                    
-                <figure className='iconeContain'>
-                    <img className='icone' src={globe} alt="icone d'un globe"></img>
-                </figure>
-                <p><a href="https://www.alwaysdata.com" target='_blank'>www.alwaysdata.com</a></p>                                                                    
-            </div>
-          
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="2">
-        <Accordion.Header>Crédits</Accordion.Header>
-        <Accordion.Body>
-          <p>Crédits</p>
-          <p>Site développé par John Doe, étudiant du CEF</p>
-          <p>Les images libres de droit sont issues du site <a href="https://pixabay.com" target='_blank'>Pixabay</a></p>
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
+                <p><a className='linkMentions' href="https://www.alwaysdata.com" target='_blank'>www.alwaysdata.com</a></p>
+              </div>          
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>Crédits</Accordion.Header>
+            <Accordion.Body className='body'>
+              <h4 className='mentionsTitle'>Crédits</h4>
+              <p>Site développé par John Doe, étudiant du CEF</p>
+              <p>Les images libres de droit sont issues du site <a className='linkMentions' href="https://pixabay.com" target='_blank'>Pixabay</a></p>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </section>
+    </Container>
   );
 }
 
